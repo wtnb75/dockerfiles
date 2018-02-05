4c7ac4d8ec37cf9e6ff845a6113ccc49bd2f13bc
Takashi WATANABE <wtnb-t@iij.ad.jp>
2018-02-05 12:05:04 +0900

    etherpad and ethercalc

---
+++ etherpad-lite/build.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+export no_proxy=$(docker-machine ip)
+args=""
+for i in $(env | grep proxy | grep -v rvm | grep -v HTTP); do
+  args="$args --build-arg $i"
+done
+
+set -x
+docker build $args -t $(basename $PWD) .
