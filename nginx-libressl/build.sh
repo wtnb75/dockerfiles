382a167def9a470d08c5cd2342ec16d3352a8d27
Takashi WATANABE <wtnb-t@iij.ad.jp>
2018-02-05 12:17:13 +0900

    add from remote url

--- nginx-libressl/build.sh
+++ nginx-libressl/build.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 
+export no_proxy=$(docker-machine ip)
 args=""
-for i in $(env | grep proxy | grep -v rvm); do
+
+for i in $(env | grep proxy | grep -v rvm | grep -v HTTP); do
   args="$args --build-arg $i"
 done
 
@@ -11,8 +13,8 @@ export LIBRESSL_VERSION=${libressl_tgz%.tar.gz}
 nginx_tgz=$(curl -sL http://nginx.org/download/ | cut -f2 -d\" | grep 'tar.gz$' | gsort -Vr | head -n1 | cut -f2 -d-)
 export NGINX_VERSION=${nginx_tgz%.tar.gz}
 
-sh download.sh
+# sh download.sh
 
 args="$args --build-arg NGINX_VERSION=${NGINX_VERSION} --build-arg LIBRESSL_VERSION=${LIBRESSL_VERSION}"
 set -x
-docker build -t nginx-libressl $args .
+docker build -t $(basename $PWD) $args .
